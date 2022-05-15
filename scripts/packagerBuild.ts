4b908437215456d2493e0878eb88a32bd2b47644
liting <1194308663@qq.com>
2022-05-15 17:06:48 +0800

    fix: 重构打包代码

--- scripts/packagerBuild.ts
+++ scripts/packagerBuild.ts
@@ -4,14 +4,18 @@ import mainConfig from './config/mainConfig';
 import DevRender from './devRender';
 import renderConfig from './config/renderConfig';
 import Base from './config/base';
+import DevPreload from './devPreload';
+import preloadConfig from './config/preloadConfig';
 
 Base.init({ renderEnv: 'electron' });
 
 const devMain = new DevMain(mainConfig);
 const devRender = new DevRender(renderConfig);
+const devPreload = new DevPreload(preloadConfig);
+
 function packagerBuild() {
   del(['./app/*', './pack/*']).then(() => {
-    Promise.all([devMain.buildMain(), devRender.buildRender()])
+    Promise.all([devMain.buildMain(), devRender.buildRender(), devPreload.buildPreload()])
       .then((res) => {
         res.forEach((item) => console.log(item));
       })
