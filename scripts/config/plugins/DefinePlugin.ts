90646c063eaca591c47829a7929a33a0de2c0439
liting <1194308663@qq.com>
2022-05-15 14:38:59 +0800

    fix: 重构打包代码

--- scripts/config/plugins/DefinePlugin.ts
+++ scripts/config/plugins/DefinePlugin.ts
@@ -1,19 +1,22 @@
 import webpack from 'webpack';
 import { version } from '../../../package.json';
-// import Base from '../base';
+import Base from '../base';
 
 
 declare global {
   const VERSION: string;
   const MODE: string;
   const APP_ENV: string;
+  const RENDER_ENV: 'browser' | 'electron';
 }
 
 export default () => {
+  const { options } = Base.getConfig();
   return new webpack.DefinePlugin({
     VERSION: JSON.stringify(version),	// 版本号
     APP_ENV: JSON.stringify(process.env.APP_ENV), // 运行的环境
     MODE: JSON.stringify(process.env.NODE_ENV), // 运行模式
+    RENDER_ENV: JSON.stringify(options.renderEnv || 'electron'),
     __VUE_PROD_DEVTOOLS__: false,
     __VUE_OPTIONS_API__: true,
   });
